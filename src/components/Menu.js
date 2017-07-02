2b46a657256998344c0db0803aeee8bb56c6b5bd
er361 <sf7kmmr@gmail.com>
2017-07-02 16:42:38 +0600

    menu done

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,17 +4,35 @@ import React, { Component } from 'react'
 import '../stylesheets/menu.scss'
 
 class Menu extends Component {
-  handleSelect(){
-    console.log('select')
+  
+  constructor(props){
+    super(props)
+
+    this.state = {
+      activeKey: 1
+    }
+
+  }
+
+  handleSelect(eventKey){
+    // console.log(eventKey)
+    this.setState({
+      activeKey: eventKey
+    })
   }
   render () {
     return (
-         <Nav bsStyle="pills" bsClass="myNav" activeKey={1} onSelect={this.handleSelect()}>
-            <NavItem className="menuButton" eventKey={1} >О компании</NavItem>
+         <Nav 
+            bsStyle="pills"
+            bsClass="myNav" 
+            activeKey={this.state.activeKey} 
+            onSelect={this.handleSelect.bind(this)}>
+
+            <NavItem eventKey={1} >О компании</NavItem>
             <NavItem eventKey={2} >Рабочий проект</NavItem>
             <NavItem eventKey={3} >Эскизный проект</NavItem>
-            <NavItem eventKey={3} >Портфолио</NavItem>
-            <NavItem eventKey={3} >Контакты</NavItem>
+            <NavItem eventKey={4} >Портфолио</NavItem>
+            <NavItem eventKey={5} >Контакты</NavItem>
         </Nav>
     )
   }
